52feaf4207486b9ee1059afa0aa44651159d0823
Vinod Kumar <vinodkumar@192.168.1.5>
2025-06-10 12:45:55 +0530

    initial commit

---
+++ src/main/java/com/codingkiddo/spring_boot_persistence_h2/SpringBootPersistenceH2Application.java
@@ -0,0 +1,13 @@
+package com.codingkiddo.spring_boot_persistence_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersistenceH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersistenceH2Application.class, args);
+	}
+
+}
